e1d42df16da7e559bbb01734101a3789b3ac4e1a
zyzyx03 <fauzuwan.nazri93@gmail.com>
2023-09-30 02:58:09 +0800

    feat(deps): add new package and set up Husky

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule as NestConfigModule} from '@nestjs/config';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [NestConfigModule.forRoot()]
+  imports: [NestConfigModule.forRoot()],
 })
 export class ConfigModule {}
